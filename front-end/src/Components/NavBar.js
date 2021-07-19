7cc65b66cdbb3c121024189781919630b1ed1492
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-19 18:32:56 -0400

    updated nav bar to match mockup and incorporated links and paths

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-        <div>
-           <h1>NavBar is Here!</h1> 
-        </div>
-    )
+  return (
+    <nav>
+      <Link to="/">
+        <h3>Home</h3>
+      </Link>
+      <Link to="/products">
+        <h3>Collection</h3>
+      </Link>
+      <Link>
+        <h3>About</h3>
+      </Link>
+    </nav>
+  );
 }
