63e7672b3be40fb7da3d30c9b9955789dd5983c3
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-19 17:51:21 -0400

    set up index page and added collection component

---
+++ front-end/src/Pages/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Collection from '../Components/Collection'
+
+export default function Index() {
+    return (
+        <div>
+        <h2>Index</h2>
+        <Collection/>
+        </div>
+    )
+}
