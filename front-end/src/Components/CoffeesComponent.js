c37c73e7a53bac9c86829c9c647b1e8bf9df9dd3
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-21 11:42:28 -0400

    trying to resolve map function issue

--- front-end/src/Components/CoffeesComponent.js
+++ front-end/src/Components/CoffeesComponent.js
@@ -22,6 +22,10 @@ export default function CoffeesComponent() {
 
   return (
     <div>
+      <br />
+      <br />
+      <br />
+      <h1>Data please</h1>
       <ol>
         {coffees.map((coffee) => {
           return (
