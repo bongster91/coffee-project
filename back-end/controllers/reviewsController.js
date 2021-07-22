0ea44e75560a1e01f0db38a99a5aabdda9a322ff
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-22 13:53:23 -0400

    reviews index route working and show route almost working

--- back-end/controllers/reviewsController.js
+++ back-end/controllers/reviewsController.js
@@ -1,16 +1,32 @@
 const express = require("express");
-const reviews = express.Router({
-    mergeParams = true
-})
+const reviews = express.Router({ mergeParams: true });
 
-const { 
-    getAllReviewsForCoffee
-  } = require("../queries/reviews");
+const { getAllReviewsForCoffee, getOneReview } = require("../queries/reviews");
 
-reviews.get("/", async (req,res)=>{
-    const allReviews = await getAllReviewsForCoffee()
-    res.status(200).json(allReviews)
-})
+//INDEX
+reviews.get("/", async (req, res) => {
+  const { coffee_id } = req.params;
+
+  const allReviews = await getAllReviewsForCoffee(coffee_id);
+  res.status(200).json(allReviews);
+});
+
+//SHOW SPECIFIC REVIEW
+reviews.get("/:id", async (req, res) => {
+//   const { coffee_id } = req.params;
+  const { id } = req.params;
+
+  try {
+    const oneReview = await getOneReview(id);
+    res.status(200).json(oneReview);
+  } catch (error) {
+    res.status(404).json({
+      error: "Not Found",
+      message: error,
+    });
+  }
+});
 
 
-  module.exports = reviews;
+
+module.exports = reviews;
--- back-end/queries/reviews.js
+++ back-end/queries/reviews.js
@@ -1,20 +1,34 @@
 const db = require("../db/dbConfig");
 
-const getAllReviewsForCoffee = async (coffeeId) => {
+//QUERY FOR ALL REVIEWS CORRESPONDING A SINGLE COFFEE PRODUCT
+const getAllReviewsForCoffee = async (coffee_id) => {
   try {
     const allReviews = await db.any(
       `SELECT * FROM reviews
         WHERE coffee_id = $1`,
-      coffeeId);
+      coffee_id
+    );
     return allReviews;
+  } catch (e) {
+    return e;
+  }
+};
+
+//QUERY TO GET A SPECIFIC REVIEW
+const getOneReview = async (id) => {
+  try {
+    const oneReview = await db.one(`SELECT * FROM reviews WHERE id =$1, id)`);
+    return oneReview;
   } catch (error) {
     return error;
   }
 };
 
-
+//QUERY TO CREATE A NEW REVIEW FOR A SPECIFC COFFEE PRODUCT 
 
 
 module.exports = {
-    getAllReviewsForCoffee,
-}
+  getAllReviewsForCoffee,
+  getOneReview,
+
+};
