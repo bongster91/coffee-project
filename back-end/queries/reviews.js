8fc6462580a58baaeb52eba10f9df68c569f6d34
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-22 21:43:33 -0400

    completed code for reviews query and controllers

--- back-end/queries/reviews.js
+++ back-end/queries/reviews.js
@@ -5,8 +5,8 @@ const getAllReviewsForCoffee = async (coffee_id) => {
   try {
     const allReviews = await db.any(
       `SELECT * FROM reviews
-        WHERE coffee_id = $1;`,
-      coffee_id
+        WHERE coffee_id = $1;`
+        , coffee_id
     );
     return allReviews;
   } catch (e) {
@@ -18,9 +18,9 @@ const getAllReviewsForCoffee = async (coffee_id) => {
 const getOneReview = async (id) => {
   try {
     const oneReview = await db.one(
-        `SELECT * FROM reviews 
-        WHERE id = $1`, 
-        id
+      `SELECT * FROM reviews 
+        WHERE id = $1;`
+        ,id
     );
     return oneReview;
   } catch (error) {
@@ -30,16 +30,48 @@ const getOneReview = async (id) => {
 
 //QUERY TO CREATE A NEW REVIEW FOR A SPECIFC COFFEE PRODUCT
 const createReview = async (review, coffee_id) => {
-
   try {
     const newReview = await db.one(
-      `INSERT INTO reviews (coffee_id, reviewer, content, rating)
-        VALUES
-        ($1, $2, $3, $4)
-        RETURNING * `,
-      [review.reviewer, review.content, review.rating, coffee_id]
+      `INSERT INTO reviews (reviewer, content, rating, coffee_id,)
+       VALUES ($1, $2, $3, $4)
+       RETURNING * ;`,
+      [
+        review.reviewer, 
+        review.content, 
+        review.rating, 
+        coffee_id
+      ]
     );
     return newReview;
+  } catch (error) {
+    return error;
+  }
+};
+
+//QUERY TO UPDATE SPECIFIC REVIEW 
+const updateReview = async (id, review) => {
+  try {
+   const updatedReview = await db.one(
+       `UPDATE reviews 
+       SET 
+       reviewer = $1, content = $2, rating = $3, coffee_id = $4
+       WHERE id = $5
+       RETURNING * ;`
+       ,id)   
+  } catch (error) {
+    return error;
+  }
+};
+
+//QUERY TO DELETE SPECIFIC REVIEW
+const deleteReview = async (id) => {
+  try {
+    const deletedReview = await db.one(
+        `DELETE FROM reviews
+        WHERE id = $1;
+        RETURNING * ;`
+        ,id)
+    return deletedReview;
   } catch (error) {
     return error;
   }
@@ -49,4 +81,6 @@ module.exports = {
   getAllReviewsForCoffee,
   getOneReview,
   createReview,
+  updateReview,
+  deleteReview,
 };
