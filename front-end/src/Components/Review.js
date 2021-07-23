7d81207e5704402e189b807f4d89642482dacc47
Grace Bong <gracebong.park@gmail.com>
2021-07-23 16:35:17 -0400

    delete review frontend added

--- front-end/src/Components/Review.js
+++ front-end/src/Components/Review.js
@@ -1,11 +1,15 @@
-import ReviewForm from './ReviewForm';
 
-function Review({ review }) {
+function Review(props) {
+    const { review, handleDelete } = props;
     return (
         <div>
+            
             <h1>{review.reviewer}</h1>
             <h2>{review.content}</h2>
             <h2>{review.rating}</h2>
+            <button onClick={() => handleDelete(review.id)}>
+                Delete
+            </button>
         </div>
     );
 };
--- front-end/src/Components/Reviews.js
+++ front-end/src/Components/Reviews.js
@@ -8,41 +8,57 @@ import ReviewForm from "./ReviewForm";
 const API = apiURL();
 
 function Reviews() {
-  let { id } = useParams();
   const [reviews, setReviews] = useState([]);
+  let { id } = useParams();
 
   useEffect(() => {
     axios
       .get(`${API}/coffees/${id}/reviews`)
       .then((res) => {
         setReviews(res.data);
-        console.log(res.data)
       });
   }, [id]);
   
-  // const handleDelete = async () => {
-  //     await axios.delete(`${apiURL}/coffees/${id}/reviews`).then(() => {
-  //         history.push(`${apiURL}/coffees/reviews`);
-  //       });
-  //   };
+  const handleDelete = async (reviewID) => {
+      await axios
+        .delete(`${API}/coffees/${id}/reviews/${reviewID}`)
+        .then(
+          (res) => {
+            const copyReviewArray = [...reviews];
+            const indexOfDeletedReview = copyReviewArray.findIndex(
+              (review) => {
+                return review.id === reviewID;
+              });
+            copyReviewArray.splice(indexOfDeletedReview, 1);
+            setReviews(copyReviewArray);
+          },
+          (e) => console.error(e)
+        )
+        .catch(
+          (c) => console.warn('catch', c)
+        );
+  };
     
     
   return (
     <section>
       <h1>Reviews</h1>
 
+      <ReviewForm />
+      <h2>Add a new review</h2>
+
       <ol>
         {reviews.map((review) => {
           return (
             <li key={review.id}>
-              <Review review={review} />
+              <Review 
+                review={review}
+                handleDelete={handleDelete}
+              />
             </li>
           )
         })}
       </ol>
-
-      <ReviewForm />
-      <h2>Add a new review</h2>
     </section>
   )
 }
