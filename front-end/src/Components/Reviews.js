8a9a1cab57c6c0ae070f9a98fbb74e115bebd3d1
cassidybeni <cassidybeni@pursuit.org>
2021-07-23 13:50:46 -0400

    Working on Reviews.js

--- front-end/src/Components/Reviews.js
+++ front-end/src/Components/Reviews.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { apiURL } from '../util/apiURL'
-import DisplayReviews from '../Components/DisplayReviews'
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { apiURL } from "../util/apiURL";
+import Review from "./Review";
 
+const apiURL = apiURL();
 
+function Reviews({ history, match }) {
+  let { id, coffees_id } = match.params;
+  const [reviews, setReviews] = useState([]);
 
-function Reviews() {
-
-    return (
-        <div>
-
-        </div>
-    )
+  useEffect(() => {
+    axios.get(`${apiURL}/coffees/${coffees_id}/reviews`).then((res) => {
+      setReviews(res.data);
+      console.log(res.data)
+    });
+  }, [id, history]);
+  
+  const handleDelete = async () => {
+      await axios.delete(`${apiURL}/coffees/${id}/reviews`).then(() => {
+          history.push(`${apiURL}/coffees/reviews`);
+        });
+    };
+    
+    
+  return <div></div>;
 }
 
 export default Reviews;
