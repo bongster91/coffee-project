822fa6629d6cdff51d9598d9028ecc9320695a85
Grace Bong <gracebong.park@gmail.com>
2021-07-23 15:00:51 -0400

    get all review frontend passing

---
+++ front-end/src/Components/ReviewForm.js
@@ -0,0 +1,11 @@
+
+
+function ReviewForm() {
+    return (
+        <div>
+            This is the Review Form page!!
+        </div>
+    );
+};
+
+export default ReviewForm;
--- front-end/src/Components/Reviews.js
+++ front-end/src/Components/Reviews.js
@@ -1,30 +1,50 @@
-import React from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { apiURL } from "../util/apiURL";
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 import Review from "./Review";
+import ReviewForm from "./ReviewForm";
 
-const apiURL = apiURL();
+const API = apiURL();
 
-function Reviews({ history, match }) {
-  let { id, coffees_id } = match.params;
+function Reviews() {
+  let { id } = useParams();
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    axios.get(`${apiURL}/coffees/${coffees_id}/reviews`).then((res) => {
-      setReviews(res.data);
-      console.log(res.data)
-    });
-  }, [id, history]);
+    axios
+      .get(`${API}/coffees/${id}/reviews`)
+      .then((res) => {
+        setReviews(res.data);
+        console.log(res.data)
+      });
+  }, [id]);
   
-  const handleDelete = async () => {
-      await axios.delete(`${apiURL}/coffees/${id}/reviews`).then(() => {
-          history.push(`${apiURL}/coffees/reviews`);
-        });
-    };
+  // const handleDelete = async () => {
+  //     await axios.delete(`${apiURL}/coffees/${id}/reviews`).then(() => {
+  //         history.push(`${apiURL}/coffees/reviews`);
+  //       });
+  //   };
     
     
-  return <div></div>;
+  return (
+    <section>
+      <h1>Reviews</h1>
+
+      <ol>
+        {reviews.map((review) => {
+          return (
+            <li key={review.id}>
+              <Review review={review} />
+            </li>
+          )
+        })}
+      </ol>
+
+      <ReviewForm />
+      <h2>Add a new review</h2>
+    </section>
+  )
 }
 
 export default Reviews;
